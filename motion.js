e0382b586a5d959668e9f812b95a528adcc8bed4
Kendall Adams <>
2021-05-28 17:43:01 +0000

    Added Box

--- motion.js
+++ motion.js
@@ -1,3 +1,18 @@
+function myBox() {
+  var elem = document.getElementById("Animation");   
+  var posTop = 0;
+  var posRight = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posTop++;
+	  posRight++;
+	  elem.style.top = posTop + "px"; 
+	  elem.style.right = posRight + "px"; 
+	  if ((posTop == 350) && (posRight == 350))
+		  clearInterval(id);
+  }
+}
+
 function myMove() {
   var elem = document.getElementById("animate");   
   var posTop = 0;
@@ -12,3 +27,4 @@ function myMove() {
 		  clearInterval(id);
   }
 }
+
